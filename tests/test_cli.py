34573e5635481c6c83d29ca7f3b7d06cfeedcde5
Fredrik Averpil <fredrik.averpil@gmail.com>
2022-02-15 22:06:52 +0100

    Support nicer paths input

---
+++ tests/test_cli.py
@@ -0,0 +1,22 @@
+import pytest
+
+from creosote import cli
+
+
+@pytest.mark.parametrize("with_packages", [["PyYAML", "protobuf"]], indirect=True)
+def test_files_as_path(capsys, with_packages):
+
+    cli.main(
+        [
+            "-p",
+            "src/creosote/formatters.py",
+            "src/creosote/models.py",
+            "-f",
+            "porcelain",
+        ]
+    )
+
+    captured = capsys.readouterr()
+    expected_log = "PyYAML\ndistlib\nprotobuf\n"
+
+    assert captured.out == expected_log
