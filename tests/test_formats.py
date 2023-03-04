b9c4cdb73592e644ecc38a8009f51a6462aac312
Fredrik Averpil <fredrik.averpil@gmail.com>
2023-03-04 16:01:52 +0100

    !feat: support pep-621

--- tests/test_formats.py
+++ tests/test_formats.py
@@ -3,10 +3,11 @@ import pytest
 from creosote import cli
 
 
-@pytest.mark.parametrize("with_packages", [["PyYAML", "protobuf"]], indirect=True)
-def test_format_porcelain(capsys, with_packages):
-
-    cli.main(["-f", "porcelain"])
+@pytest.mark.parametrize(
+    "with_poetry_packages", [["PyYAML", "protobuf"]], indirect=True
+)
+def test_format_porcelain(capsys, with_poetry_packages):
+    cli.main(["-f", "porcelain", "-s", "tool.poetry.dependencies"])
 
     captured = capsys.readouterr()
     expected_log = "protobuf\npyyaml\n"
