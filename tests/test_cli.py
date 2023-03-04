3cf7c0b7cbec4e56031dddf8b761cfbfbe97a9c9
Fredrik Averpil <fredrik.averpil@gmail.com>
2023-03-04 18:00:05 +0100

    feat: add git link support

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,3 +1,4 @@
+
 import pytest
 
 from creosote import cli
@@ -20,7 +21,7 @@ def test_files_as_path_poetry(capsys, with_poetry_packages):
     )
 
     captured = capsys.readouterr()
-    expected_log = "distlib\nprotobuf\npyyaml\ntoml\n"
+    expected_log = "distlib\ndotty-dict\nprotobuf\npyyaml\ntoml\n"
 
     assert captured.out == expected_log
 
@@ -40,5 +41,25 @@ def test_files_as_path_pep621(capsys):
 
     captured = capsys.readouterr()
     expected_log = "distlib\ndotty-dict\ntoml\n"
+
+    assert captured.out == expected_log
+
+
+@pytest.mark.parametrize("with_pip_packages", [["boto3"]], indirect=True)
+def test_files_as_path_pep621_git(capsys, with_pip_packages):
+    cli.main(
+        [
+            "-p",
+            "src/creosote/formatters.py",
+            "src/creosote/models.py",
+            "-f",
+            "porcelain",
+            "-s",
+            "project.optional-dependencies.test-git-link",
+        ]
+    )
+
+    captured = capsys.readouterr()
+    expected_log = "boto3\n"
 
     assert captured.out == expected_log
--- tests/test_parsers.py
+++ tests/test_parsers.py
@@ -18,3 +18,18 @@ def test_dependency_without_version_constraint(dependency_string, expected_packa
     assert expected_package == PackageReader.dependency_without_version_constraint(
         dependency_string
     )
+
+
+@pytest.mark.parametrize(
+    ["dependency_string", "expected_package"],
+    [
+        ("pip @ file:///localbuilds/pip-1.3.1.zip", "pip"),
+        ("pip @ https://github.com/pypa/pip.git", "pip"),
+        ("pip @ git+https://github.com/pypa/pip.git", "pip"),
+        ("pip @ git+ssh://github.com/pypa/pip.git", "pip"),
+    ],
+)
+def test_pyproject_directref_package_name(dependency_string, expected_package):
+    assert expected_package == PackageReader.dependency_without_direct_reference(
+        dependency_string
+    )
