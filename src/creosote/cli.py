0ca758a783eca2148b8d8376ab1ba8ba279a92d7
Fredrik Averpil <fredrik.averpil@gmail.com>
2023-04-01 15:10:57 +0200

    feat: add new args setup behind feature

--- src/creosote/cli.py
+++ src/creosote/cli.py
@@ -13,6 +13,7 @@ class Features(Enum):
     """Features that can be enabled via the --use-feature flag."""
 
     FAIL_EXCLUDED_AND_NOT_INSTALLED = "fail-excluded-and-not-installed"
+    ARGS_CAN_BE_SPECIFIED_MULTIPLE_TIMES = "v3-args"
 
 
 class CustomAppendAction(argparse.Action):
@@ -74,23 +75,65 @@ def parse_args(args):
         choices=["default", "porcelain"],
         help="output format",
     )
-    parser.add_argument(
-        "-p",
-        "--paths",
-        dest="paths",
-        default=glob.glob("src"),
-        nargs="*",
-        help="paths(s) to Python source code to scan for imports",
-    )
-    parser.add_argument(
-        "-v",
-        "--venv",
-        dest="venvs",
-        metavar="PATH",
-        action=CustomAppendAction,
-        default=[".venv"],
-        help="path to the virtual environment to scan for dependencies",
-    )
+
+    if Features.ARGS_CAN_BE_SPECIFIED_MULTIPLE_TIMES.value in sys.argv:
+        # v3.0 behavior
+        parser.add_argument(
+            "-p",
+            "--path",
+            dest="paths",
+            metavar="PATH",
+            action=CustomAppendAction,
+            default=["src"],
+            help="paths(s) to Python source code to scan for imports",
+        )
+        parser.add_argument(
+            "-s",
+            "--section",
+            dest="sections",
+            metavar="TOML_SECTION",
+            action=CustomAppendAction,
+            default=["project.dependencies"],
+            help="pyproject.toml section(s) to scan for dependencies",
+        )
+        parser.add_argument(
+            "--exclude-dep",
+            dest="exclude_deps",
+            metavar="DEPENDENCY",
+            action="append",
+            default=[],
+            help="dependencies to exclude from the scan",
+        )
+    else:
+        # v2.x behavior
+        parser.add_argument(
+            "-p",
+            "--paths",
+            dest="paths",
+            default=glob.glob("src"),
+            nargs="*",
+            help="paths(s) to Python source code to scan for imports",
+        )
+        parser.add_argument(
+            "-s",
+            "--sections",
+            dest="sections",
+            metavar="TOML_SECTION",
+            nargs="*",
+            default=["project.dependencies"],
+            help="pyproject.toml section(s) to scan for dependencies",
+        )
+        parser.add_argument(
+            "--exclude-deps",
+            dest="exclude_deps",
+            metavar="DEPENDENCY",
+            nargs="*",
+            default=[],
+            help="dependencies to exclude from the scan",
+        )
+
+    # TODO: Add support for supplying a list of deps files, which
+    # are coupled with the section(s) to read...?
     parser.add_argument(
         "-d",
         "--deps-file",
@@ -100,21 +143,13 @@ def parse_args(args):
         help="path to the pyproject.toml or requirements[.txt|.in] file",
     )
     parser.add_argument(
-        "-s",
-        "--sections",
-        dest="sections",
-        metavar="SECTION",
-        nargs="*",
-        default=["project.dependencies"],
-        help="pyproject.toml section(s) to scan for dependencies",
-    )
-    parser.add_argument(
-        "--exclude-deps",
-        dest="exclude_deps",
-        metavar="DEPENDENCY",
-        nargs="*",
-        default=[],
-        help="dependencies to exclude from the scan",
+        "-v",
+        "--venv",
+        dest="venvs",
+        metavar="PATH",
+        action=CustomAppendAction,
+        default=[".venv"],
+        help="path to the virtual environment to scan",
     )
     parser.add_argument(
         "--use-feature",
