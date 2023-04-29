21e70d6f873e544dc798ba766d220bdf8c85e743
Fredrik Averpil <fredrik.averpil@gmail.com>
2023-04-29 21:59:25 +0200

    fix: ruff

--- tests/fixtures/manual.py
+++ tests/fixtures/manual.py
@@ -22,7 +22,7 @@ def with_poetry_packages(_stash_away_project_toml, capsys, request):
     with capsys.disabled():
         Path(deps_files / "pyproject.poetry.toml").rename(repo_root / "pyproject.toml")
         subprocess.run(
-            ["poetry", "add", *request.param],
+            ["poetry", "add", *request.param],  # noqa: S603, S607
             stdout=subprocess.DEVNULL,
         )
     try:
@@ -30,7 +30,7 @@ def with_poetry_packages(_stash_away_project_toml, capsys, request):
     finally:
         with capsys.disabled():
             subprocess.run(
-                ["poetry", "remove", *request.param],
+                ["poetry", "remove", *request.param],  # noqa: S603, S607
                 stdout=subprocess.DEVNULL,
             )
             pathlib.Path("poetry.lock").unlink()
@@ -47,7 +47,7 @@ def with_pyproject_pep621_packages(_stash_away_project_toml, capsys, request):
     with capsys.disabled():
         Path(deps_files / "pyproject.pep621.toml").rename(repo_root / "pyproject.toml")
         subprocess.run(
-            ["pip", "install", *request.param],
+            ["pip", "install", *request.param],  # noqa: S603, S607
             stdout=subprocess.DEVNULL,
         )
     try:
@@ -55,7 +55,7 @@ def with_pyproject_pep621_packages(_stash_away_project_toml, capsys, request):
     finally:
         with capsys.disabled():
             subprocess.run(
-                ["pip", "uninstall", "-y", *request.param],
+                ["pip", "uninstall", "-y", *request.param],  # noqa: S603, S607
                 stdout=subprocess.DEVNULL,
             )
             Path(repo_root / "pyproject.toml").rename(
@@ -71,7 +71,7 @@ def with_requirements_txt_packages(_stash_away_project_toml, capsys, request):
     with capsys.disabled():
         Path(deps_files / "requirements.txt").rename(repo_root / "requirements.txt")
         subprocess.run(
-            ["pip", "install", *request.param],
+            ["pip", "install", *request.param],  # noqa: S603, S607
             stdout=subprocess.DEVNULL,
         )
     try:
@@ -79,7 +79,7 @@ def with_requirements_txt_packages(_stash_away_project_toml, capsys, request):
     finally:
         with capsys.disabled():
             subprocess.run(
-                ["pip", "uninstall", "-y", *request.param],
+                ["pip", "uninstall", "-y", *request.param],  # noqa: S603, S607
                 stdout=subprocess.DEVNULL,
             )
             Path(repo_root / "requirements.txt").rename(deps_files / "requirements.txt")
