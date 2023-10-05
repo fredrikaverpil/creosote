4dc0d3bb8c9f73a25bda13f16e85a51ecb7387e0
Brian Pugh <bnp117@gmail.com>
2023-10-04 18:25:29 -0700

    fix CustomAppendAction incompatabilities with parser.set_defaults

--- src/creosote/config.py
+++ src/creosote/config.py
@@ -51,14 +51,13 @@ class CustomAppendAction(argparse.Action):
     def __init__(self, option_strings, dest, nargs=None, **kwargs):
         """Initialize the action."""
         self.called_times = 0
-        self.default_value = kwargs.get("default")
         super().__init__(option_strings, dest, **kwargs)
 
     def __call__(self, parser, namespace, values, option_string=None):
         """When the argument is specified on the commandline."""
         current_values = getattr(namespace, self.dest)
 
-        if self.called_times == 0 and current_values == self.default_value:
+        if self.called_times == 0:
             current_values = []
 
         current_values.append(values)
